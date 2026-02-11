b722a87c8f5f8640ce7b8273d4a3393f81daf25f
Kisakye Jonah W <125368313+Jhonhah@users.noreply.github.com>
2026-02-10 19:37:00 -0800

    words switch with button click

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 
 from tkinter import *
+from pandas import *
+from random import choice
+
+#-----------------------------DATA SETUP----------------------------#
+data = read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+#-----------------------------SWITCH WORD----------------------------#
+def switch_word():
+    new_word = choice(to_learn)
+    canvas.itemconfig(word, text=new_word["French"])
+    canvas.itemconfig(title, text="French")
 
 #-----------------------------UI SETUP----------------------------#
 window = Tk()
@@ -12,22 +24,20 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=img)
-canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"), fill="black")
-canvas.create_text(400, 263, text="partie", font=("Ariel", 60, "bold"), fill="black")
+title = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
+word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 #left button
 cross_img = PhotoImage(file="images/wrong.png")
-left_button = Button(image=cross_img, highlightthickness=0, bg=BACKGROUND_COLOR, command="")
+left_button = Button(image=cross_img, highlightthickness=0, command=switch_word)
 left_button.grid(column=0, row=1)
 
 #right button
 check_img = PhotoImage(file="images/right.png")
-right_button = Button(image=check_img, highlightthickness=0, bg=BACKGROUND_COLOR, command="")
+right_button = Button(image=check_img, highlightthickness=0, command=switch_word)
 right_button.grid(column=1, row=1)
 
-
-
-
+switch_word()
 
 window.mainloop()
